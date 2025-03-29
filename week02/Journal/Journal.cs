acaea7618acded0f23a6645337c9a84d73df9b4c
jmcarcel <carceljoan@gmail.com>
2025-03-29 12:29:40 -0300

    journal assigment

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,48 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+    
+    public Journal()
+    {
+
+    }
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAllEntries()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string file)
+    {
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine(entry);
+            }
+        }
+    }
+    public void LoadFromFile(string file)
+    {
+        string[] lines = System.IO.File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(",");
+
+            Entry newEntry = new Entry();
+            newEntry._date = parts[0];
+            newEntry._promptText = parts[1];
+            newEntry._entryText = parts[2];
+
+            _entries.Add(newEntry);
+        }
+    }
+}
