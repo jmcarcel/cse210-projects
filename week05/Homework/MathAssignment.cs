fbf3060a1149ae5ceafa8dfb9e4522144c9e9dca
jmcarcel <carceljoan@gmail.com>
2025-08-14 20:26:06 -0300

    assignment 07

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,8 @@
+public class MathAssignment
+{
+    private string _studentName;
+    private string _topic;
+    private string _textbookSection;
+    private string _problems;
+
+}
---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,6 @@
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running()
+}
